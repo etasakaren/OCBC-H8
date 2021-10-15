1feca41cd694f0fbccc93f95c3f2bc228c2f4561
Etasa K <ekarenisa@yahoo.co.id>
2021-10-15 11:35:02 +0700

    sesi 9

---
+++ Sesi 9/Kantor_WebAPI/Kantor_WebAPI/Models/EmployeeContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Kantor_WebAPI.Models
+{
+    public class EmployeeContext
+    {
+        public string connectionString { get; set; }
+
+        public EmployeeContext(string connectionStr)
+        {
+            this.connectionString = connectionStr;
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+
+        public List<EmployeeItem> GetAllEmployee()
+        {
+            List<EmployeeItem> list = new List<EmployeeItem>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from employee", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new EmployeeItem()
+                        {
+                            id = reader.GetInt32("id"),
+                            nama = reader.GetString("nama"),
+                            jenisKelamin = reader.GetString("jenis_kelamin"),
+                            alamat = reader.GetString("alamat")
+                        });
+                    }
+                    
+                }
+            }
+            return list;
+        }
+
+        public List<EmployeeItem> GetEmployee(string id)
+        {
+            List<EmployeeItem> list = new List<EmployeeItem>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from employee where id = @id", conn);
+                cmd.Parameters.AddWithValue("@id", id);
+
+                using(MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new EmployeeItem()
+                        {
+                            id = reader.GetInt32("id"),
+                            nama = reader.GetString("nama"),
+                            jenisKelamin = reader.GetString("jenis_kelamin"),
+                            alamat = reader.GetString("alamat")
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
